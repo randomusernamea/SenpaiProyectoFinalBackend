e61174d5b65ae04612b1b3fe4115598538802d12
randomusernamea <stars.a@outlook.com>
2023-02-14 21:49:55 -0300

    Cambio al espaniol

--- controllers/pokedex.js
+++ controllers/pokedex.js
@@ -6,7 +6,7 @@ SECRET_KEY="IENB(#HYie-igh*)Ihtgq10b";
 
 exports.login = (req,res) => {
     const {username, password} = req.body;
-    knex("users").select("*").where("username", username).then(
+    knex("usuarios").select("*").where("correo", username).then(
         function(data){
             if(data.length != 1) {
                 res.status(400).json("Usuario o contrasena incorrectos")
@@ -27,13 +27,13 @@ exports.login = (req,res) => {
       permisos = Number(permisos)
       const salt = bcrypt.genSaltSync(12)
       const passHash = bcrypt.hashSync(password, salt)
-      knex("ususuarios").select("*").where("correo", username).then(
+      knex("usuarios").select("*").where("correo", username).then(
           function(data){
               if (data.length != 0){
                   res.status(400).json({error:"Usuario ya registrado"})
               }
               else {
-                  knex("users").insert({username: username, password: passHash, permisos: permisos})
+                  knex("usuarios").insert({username: username, password: passHash, permisos: permisos})
               .then(function(data){
               res.status(200).send(data)
           })
