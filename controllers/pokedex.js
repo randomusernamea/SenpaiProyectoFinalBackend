2783a8cada4413c5cfd40493eeadbfdca0ec3b7b
Joaquin Alonso <38812365+Joalo95@users.noreply.github.com>
2023-02-16 00:47:58 +0000

    Agrego controller Pokemon
    agrego endpoint /add, /update, /delete

--- controllers/pokedex.js
+++ controllers/pokedex.js
@@ -1,124 +1,158 @@
 const knex = require("../knexfile");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 SECRET_KEY = "IENB(#HYie-igh*)Ihtgq10b";
 
-exports.mostrarPokemones = (req, res) => {
-  knex("Pokemones")
-    .then((resultado) => {
-      res.status(200).json(resultado);
-    })
-    .catch((error) => {
-      res.status(400).json({ error: error.message });
-    });
-};
-
-exports.buscarPokemon = (req, res) => {
-  const id = Number(req.params.id);
-  knex("Pokemones")
-    .where("id", id)
-    .then((resultado) => {
-      res.status(200).json(resultado);
-    })
-    .catch((error) => {
-      res.status(400).json({ error: error.message });
-    });
-};
-
-exports.login = (req, res) => {
-  const { username, password } = req.body;
-  knex("usuarios")
-    .select("*")
-    .where("correo", username)
-    .then(function (data) {
-      if (data.length != 1) {
-        res.status(400).json("Usuario o contrasena incorrectos");
-      }
-      const user = data[0];
-      const validPassword = bcrypt.compareSync(password, user.password);
-      if (validPassword) {
-        const token = jwt.sign(
-          {
-            username: user.username,
-            permisos: user.permisos,
-            date: Date.now(),
-          },
-          SECRET_KEY
-        );
-        res.status(200).json({ error: null, data: "Login exitoso", token });
-      } else {
-        return res
-          .status(400)
-          .json({ error: "Usuario o contrasena incorrectos" });
-      }
-    });
-};
-
-exports.register = (req, res) => {
-  let { username, password, permisos } = req.body;
-  permisos = Number(permisos);
-  const salt = bcrypt.genSaltSync(12);
-  const passHash = bcrypt.hashSync(password, salt);
-  knex("usuarios")
-    .select("*")
-    .where("correo", username)
-    .then(function (data) {
-      if (data.length != 0) {
-        res.status(400).json({ error: "Usuario ya registrado" });
-      } else {
-        knex("usuarios")
-          .insert({
-            username: username,
-            password: passHash,
-            permisos: permisos,
-          })
-          .then(function (data) {
-            res.status(200).send(data);
-          });
-      }
-    });
-};
-
-exports.addPokemon = (req, res) => {
-  const { tipo_id, nombre, foto, peso, altura, habilidad, descripcion } =
-    req.body;
-  knex("Pokemones")
-    .insert({
-      tipo_id: tipo_id,
-      nombre: nombre,
-      foto: foto,
-      peso: peso,
-      altura: altura,
-      habilidad: habilidad,
-      descripcion: descripcion,
-    })
-    .then(() => {
-      knex("Pokemones")
-        .select()
-        .then((pokemones) => {
-          res.status(200).json({ message: `Se agrego el pokemon ${nombre}` });
+exports.mostrarPokemones = async (req, res) => {
+    await knex("Pokemones")
+        .join('tipos', 'pokemones.tipo_id', '=', 'tipos.id')
+        .join('estadisticas', 'pokemones.id', '=', 'estadisticas.id')
+        .then((resultado) => {
+            res.status(200).json(resultado);
+        })
+        .catch((error) => {
+            res.status(400).json({ error: error.message });
         });
-    })
-    .catch((error) => {
-      res.status(400).json({ error: error.message });
-    });
 };
 
-exports.deletePokemon = (req, res) => {
-  const id = req.params.id;
-  knex("Pokemones")
-    .where("id", id)
-    .del()
-    .then(() => {
-      knex("Pokemones")
-        .select()
-        .then((pokemones) => {
-          res
-            .status(200)
-            .json({ message: `Se elimino el pokemon ${id}`, pokemones });
+exports.addPokemon = async (req, res) => {
+    const pokemon = req.body
+    await knex("Pokemones")
+        .insert({
+            tipo_id: pokemon.id,
+            nombre: pokemon.nombre,
+            peso: pokemon.peso,
+            altura: pokemon.altura,
+            habilidad: pokemon.habilidad,
+            descripcion: pokemon.descripcion
+        })
+        .then(() => {
+            res.status(200).json({ error: null, data: "Se agrego correctamente", pokemon })
+        })
+        .catch((error) => {
+            res.status(400).json({ error: error.message })
+        })
+}
+
+exports.addTipoPokemon = async (req, res) => {
+    const tipo = req.body
+    await knex("Tipos")
+        .insert({
+            id: tipo.id,
+            nombre: tipo.nombre
+        })
+        .then(() => {
+            res.status(200).json({ error: null, data: "Se agrego correctamente", tipo })
+        })
+        .catch((error) => {
+            res.status(400).json({ error: error.message })
+        })
+}
+
+exports.addEstadisticaPokemon = async (req, res) => {
+    const estadistica = req.body
+    await knex("Estadisticas")
+        .insert({
+            id: estadistica.id,
+            hp: estadistica.hp,
+            atk: estadistica.atk,
+            def: estadistica.def,
+            satk: estadistica.satk,
+            sdef: estadistica.sdef,
+            spd: estadistica.spd
+        })
+        .then(() => {
+            res.status(200).json({ error: null, data: "Se agrego correctamente", estadistica })
+        })
+        .catch((error) => {
+            res.status(400).json({ error: error.message })
+        })
+}
+
+exports.deletePokemon = async (req, res) => {
+    await knex("Pokemones")
+        .where("id", Number(req.params.id))
+        .del()
+        .then(() => {
+            res.status(200).json({ message: "borrado correctamente" })
+        })
+        .catch((error) => {
+            res.status(400).json({ error: error.message })
+        })
+}
+
+exports.updatePokemon = async (req, res) => {
+    const pokemon = req.body
+    await knex("Pokemones")
+        .where("id", req.params.id)
+        .update({
+            tipo_id: pokemon.id,
+            nombre: pokemon.nombre,
+            peso: pokemon.peso,
+            altura: pokemon.altura,
+            habilidad: pokemon.habilidad,
+            descripcion: pokemon.descripcion
+        }
+        )
+        .then(() => {
+            res.status(200).json({ message: "modificado correctamente" })
+        })
+        .catch((error) => {
+            res.status(400).json({ error: error.message })
+        })
+}
+
+
+exports.login = async (req, res) => {
+    const { correo, password } = req.body;
+    await knex("Usuarios").select("*").where("correo", correo).then(
+        function (data) {
+            if (data.length != 1) {
+                res.status(400).json("Usuario o contrasena incorrectos")
+            }
+            const user = data[0]
+            const validPassword = bcrypt.compareSync(password, user.clave)
+            if (validPassword) {
+                const token = jwt.sign({
+                    nombre: user.nombre, permisos: user.permisos, date: Date.now()
+                }, SECRET_KEY);
+                res.status(200).json({ error: null, data: 'Login exitoso', token })
+            }
+            else { return res.status(400).json({ error: "Usuario o contrasena incorrectos" }) }
+        }
+    )
+}
+
+exports.logout = (req, res) => {
+    if (req.session) {
+        req.session.delete(err => {
+            if (err) {
+                res.status(400).send('Unable to log out')
+            } else {
+                res.send('Logout successful')
+            }
         });
-    })
-    .catch((error) => {
-      res.status(400).json({ error: error.message });
-    });
-};
+    } else {
+        res.end()
+    }
+}
+
+exports.register = async (req, res) => {
+    let { nombre, correo, clave, permisos } = req.body
+    permisos = Number(permisos)
+    const salt = await bcrypt.genSaltSync(12)
+    const passHash = await bcrypt.hashSync(clave, salt)
+    await knex("Usuarios").select("*").where("correo", correo).then(
+        function (data) {
+            if (data.length != 0) {
+                res.status(400).json({ error: "Usuario ya registrado" })
+            }
+            else {
+                knex("Usuarios").insert({ nombre: nombre, correo: correo, clave: passHash, permisos: permisos })
+                    .then(function (data) {
+                        res.status(200).send(data)
+                    })
+            }
+        }
+    )
+}
