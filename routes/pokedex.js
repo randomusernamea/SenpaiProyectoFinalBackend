af7e406dd86bdf34f3bd073d78a6c3d4aabd024d
Joaquin Alonso <38812365+Joalo95@users.noreply.github.com>
2023-02-17 00:06:06 +0000

    Agrego routes con los nuevos validators agregados

--- routes/pokedex.js
+++ routes/pokedex.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
-const { register, login, mostrarPokemones, logout, addPokemon, updatePokemon, deletePokemon, addTipoPokemon, addEstadisticaPokemon } = require('../controllers/pokedex')
-const { claveValido, permisosValido, runValidate, correoValido } = require('../validators/middleware')
+const { register, login, mostrarPokemones, logout, addPokemon, updatePokemon, deletePokemon, addTipoPokemon, addEstadisticaPokemon, updateEstadistica } = require('../controllers/pokedex')
+const { usuarioValido, passwordValido, permisosValido, runValidate, correoValido, pokemonValidator, estadisticaValidator, tipoPokemonValidator } = require('../validators/middleware')
 
 //Routes Pokemones
 router.get("/listaPokemones", mostrarPokemones);
-router.post("/pokemon/nuevo", addPokemon);
-router.put("/pokemo/editar", updatePokemon);
+router.post("/pokemon/nuevo", pokemonValidator, runValidate, addPokemon);
+router.put("/pokemon/editar", pokemonValidator, runValidate, updatePokemon);
 router.delete("/pokemon/eliminar/:id", deletePokemon);
-router.post("/estadistica/nuevo", addEstadisticaPokemon);
-router.post("/tipo/nuevo", addTipoPokemon);
+router.post("/estadistica/nuevo", estadisticaValidator, runValidate, addEstadisticaPokemon);
+router.put("/estadistica/editar", estadisticaValidator, runValidate, updateEstadistica);
+router.post("/tipo/nuevo", tipoPokemonValidator, runValidate, addTipoPokemon);
 
 //Routes Users
-router.post("/login", correoValido, claveValido, runValidate, login);
+router.post("/login", correoValido, passwordValido, runValidate, login);
 router.post("/logout", logout);
-router.post("/register", correoValido, correoValido, claveValido, permisosValido, runValidate, register);
+router.post("/register", usuarioValido, correoValido, passwordValido, permisosValido, runValidate, register);
 
 
-module.exports = router
