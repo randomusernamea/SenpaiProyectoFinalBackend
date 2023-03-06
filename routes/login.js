d532ae6702bc0f9b48f1368b6fbe079b7fed3fc4
randomusernamea <stars.a@outlook.com>
2023-03-06 20:02:37 -0300

    Middleware Nombre

--- routes/login.js
+++ routes/login.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const { register, login, logout} = require('../controllers/login')
-const { claveValido, permisosValido, correoValido } = require('../validators/login')
+const { nombreValido, claveValido, permisosValido, correoValido } = require('../validators/login')
 const {runValidate} = require('../validators/middleware')
 
-router.post("/register", correoValido, correoValido, claveValido, permisosValido, runValidate, register);
+router.post("/register", nombreValido, correoValido, claveValido, permisosValido, runValidate, register);
 router.post("/login", correoValido, claveValido, runValidate, login);
 router.post("/logout", logout);
 
