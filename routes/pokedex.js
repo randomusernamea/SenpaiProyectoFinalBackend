e0de296aaba8da8508a66ca583592fffc258a44d
randomusernamea <stars.a@outlook.com>
2023-03-10 19:44:15 -0300

    Descomentar isAdmin

--- routes/pokedex.js
+++ routes/pokedex.js
@@ -24,7 +24,7 @@ const upload = multer({ storage: storage });
 //Routes Pokemones
 router.get("/pokedex", mostrarPokemones);
 router.post("/pokemon/nuevo", upload.single('Imagen'),reparseFormToBody, verifyToken, isAdmin, imagenNoExiste,pokemonNombreValido, pokemonIdValido, pokemonHpValido, pokemonAtkValido, pokemonDefValido, pokemonSpdValido, pokemonSatkValido, pokemonSdefValido, pokemonHeightValido, pokemonWeightValido, runValidate, pokemonTipoValido, addPokemon);
-router.put("/pokemon/editar", upload.single('Imagen'), reparseFormToBody, /*verifyToken, isAdmin ,*/imagenExiste, pokemonNombreValido, pokemonIdValido, pokemonHpValido, pokemonAtkValido, pokemonDefValido, pokemonSpdValido, pokemonSatkValido, pokemonSdefValido, pokemonHeightValido, pokemonWeightValido, runValidate, pokemonTipoValido, updatePokemon);
+router.put("/pokemon/editar", upload.single('Imagen'), reparseFormToBody, verifyToken, isAdmin ,imagenExiste, pokemonNombreValido, pokemonIdValido, pokemonHpValido, pokemonAtkValido, pokemonDefValido, pokemonSpdValido, pokemonSatkValido, pokemonSdefValido, pokemonHeightValido, pokemonWeightValido, runValidate, pokemonTipoValido, updatePokemon);
 router.get("/pokedex/:id", mostrarPokemonId)
 router.delete("/pokemon/eliminar/:id", deletePokemon);
 router.post("/tipos/nuevo", pokemonTipoValido, isAdmin, addTipo);
